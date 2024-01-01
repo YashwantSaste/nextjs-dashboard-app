59e9b554281f82832d08730806205067892163f0
YashwantSaste <2021bec122@sggs.ac.in>
2024-01-01 09:23:59 +0530

    Adding the skeleton loading of the dashboard page

---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,7 @@
+import DashboardSkeleton from "../ui/skeletons"
+export default function Loading(){
+    return(
+        // <div>Loading...</div> -->this was the first loading screen code and now we will add the skeleton loading
+        <DashboardSkeleton/>
+    )
+}
